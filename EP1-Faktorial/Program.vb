07fd49b991bed33b2b109030cd5602f745df03cb
Kateřina Sedláková <Sedlakova.Katerina@stud.vassboskovice.cz>
2024-05-13 13:19:06 +0200

    fdyg

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -1,25 +1,32 @@
 ﻿Imports System
 
-Module Program
-    Sub Main(args As String())
-        Dim cislo As Integer
-        Dim soucet As Integer = 0
 
-        Console.Write("Zadej èíslo: ")
-        cislo = Console.ReadLine
-        If cislo <= 0 Then
-            Console.Write("Chyba vstupu!")
-            End
-        End If
+Sub Main(args As String())
+    Dim cislo As Integer
+    Dim soucet As Integer = 0
 
+    Console.Write("Zadej èíslo: ")
+    cislo = Console.ReadLine
+    If cislo <= 0 Then
+        Console.Write("Chyba vstupu")
+        End
+    End If
 
-        For i = 1 To cislo
-            soucet = soucet + i
-        Next
+    If cislo > 10 Then
+        Console.WriteLine("musi byt mensi nez deset")
+        Console.ReadKey()
+        End
 
-        Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
+    End If
+    If Not IsNumeric(cislo) Then
+        Console.WriteLine("neni to cislo")
+        Console.ReadKey()
+        End
+    End If
+    For i = 1 To cislo
+        soucet = soucet + i
+    Next
 
-    End Sub
-End Module
+    Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
 
 

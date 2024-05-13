3bf521227ac08eff9673c1b47d4abb815ca89b7c
Kateřina Sedláková <Sedlakova.Katerina@stud.vassboskovice.cz>
2024-05-13 13:42:39 +0200

    gjhb

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -1,32 +1,35 @@
 ﻿Imports System
 
+Module Program
+    Sub Main(args As String())
+        Dim cislo As Integer
+        Dim soucet As Integer = 0
 
-Sub Main(args As String())
-    Dim cislo As Integer
-    Dim soucet As Integer = 0
+        Console.Write("Zadej èíslo: ")
+        cislo = Console.ReadLine
+        If cislo <= 0 Then
+            Console.Write("Chyba vstupu")
+            End
+        End If
 
-    Console.Write("Zadej èíslo: ")
-    cislo = Console.ReadLine
-    If cislo <= 0 Then
-        Console.Write("Chyba vstupu")
-        End
-    End If
+        If cislo > 10 Then
+            Console.WriteLine("musi byt mensi nez deset")
+            Console.ReadKey()
+            End
 
-    If cislo > 10 Then
-        Console.WriteLine("musi byt mensi nez deset")
-        Console.ReadKey()
-        End
+        End If
+        If Not IsNumeric(cislo) Then
+            Console.WriteLine("neni to cislo")
+            Console.ReadKey()
+            End
+        End If
+        For i = 1 To cislo
+            soucet = soucet + i
+        Next
 
-    End If
-    If Not IsNumeric(cislo) Then
-        Console.WriteLine("neni to cislo")
-        Console.ReadKey()
-        End
-    End If
-    For i = 1 To cislo
-        soucet = soucet + i
-    Next
+        Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
 
-    Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
+    End Sub
+End Module
 
 
